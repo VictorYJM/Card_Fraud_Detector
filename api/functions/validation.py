345688cdece14c354d7e5c69ddfc829038a50dca
its-jp <jpvalderramasantos@gmail.com>
2025-11-19 09:51:16 -0300

    adding part of the model implementation

--- api/functions/validation.py
+++ api/functions/validation.py
@@ -1,4 +1,11 @@
 from api.models.TransactionRecord import TransactionRecord
+import pickle
 
+model : any
+
+with open('../ai_model/et_model.pkl') as f:
+    model = pickle.load(f)
+    
 def is_fraud(tx: TransactionRecord) -> bool:
+
     return False
